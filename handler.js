77ba5de968a706377a06c7f7cb498e9a9ec66326
Mark Rahman <mark@boo2.nl>
2022-05-06 22:38:15 +0600

    data saved to s3 bucket

--- handler.js
+++ handler.js
@@ -3,8 +3,13 @@
 const { Client, Connection } = require("@opensearch-project/opensearch");
 const { defaultProvider } = require("@aws-sdk/credential-provider-node");
 const aws4 = require("aws4");
+const AWS = require('aws-sdk');
 
 module.exports.start = async (event) => {
+
+  const items = await getQueryItems( process.env.SEARCHINDEX, process.env.DELETEAFTER );
+
+  await saveToS3( items );
 
   const result = await deleteWithRange(process.env.SEARCHINDEX, process.env.DELETEAFTER );
   return {
@@ -12,10 +17,7 @@ module.exports.start = async (event) => {
     body: JSON.stringify(
       result
     ),
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  }; 
 };
 
 const createAwsConnector = (credentials, region) => {
@@ -48,6 +50,32 @@ const getClient = async () => {
   
 }
 
+//get the items before delete 
+
+async function getQueryItems(searchIndex, deleteAfter){
+  var client = await getClient(); 
+  try{
+    const items = await client.search({
+      index: searchIndex, 
+      body:{
+        query: {
+          range:{
+            updated_at:{
+              lte: deleteAfter
+            }
+          }
+        }
+      }
+    });
+
+    return items?.body?.hits?.hits ; 
+
+  }catch( error ){
+    console.log( error );
+    return [] ; 
+  }
+}
+
 async function deleteWithRange( searchIndex, deleteAfter ){
   var client = await getClient();
   try{
@@ -71,4 +99,27 @@ async function deleteWithRange( searchIndex, deleteAfter ){
     console.log( error );
   }
 
+}
+
+async function saveToS3(data) {
+  const s3 = new AWS.S3({});
+  const today = new Date();
+
+  const filename = 'deletedat-' + today.getDate() + '-' + (today.getMonth() + 1 ) + '-' + today.getFullYear() ; 
+
+  // console.log( filename );
+  const bucketName = process.env.AWS_BUCKET_NAME ; 
+
+  const params = {
+      Bucket: bucketName,
+      Key: filename +'.json',
+      Body: JSON.stringify(data),
+      ContentType: 'application/json',
+  };
+
+  const uploadedDocument = await s3.upload( params ).promise(); 
+
+  console.log( uploadedDocument.Location );
+
+  return uploadedDocument?.Location ; 
 }
